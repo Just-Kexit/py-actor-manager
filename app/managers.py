0a3ec5340574e84b5444dbad354c8b9fbdbe7a16
Bohdan Mykyta <justkexitmail@gmail.com>
2025-06-01 06:55:32 +0300

    Success!

--- app/managers.py
+++ app/managers.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+from string import Template
 from app.models import Actor
 
 
@@ -18,16 +18,19 @@ class ActorManager:
             first_name: str,
             last_name: str
     ) -> None:
+        query = Template(
+            "INSERT INTO $table (first_name, last_name) VALUES (?, ?)"
+        )
         self._connection.execute(
-            f"INSERT INTO {self.table_name}"
-            " (first_name, last_name) VALUES (?, ?)",
+            query.substitute(table=self.table_name),
             (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list:
+        query = Template("SELECT * FROM $table")
         actor_table_list = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
+            query.substitute(table=self.table_name)
         )
         return [
             Actor(*row) for row in actor_table_list
@@ -39,17 +42,19 @@ class ActorManager:
             new_first_name: str,
             new_last_name: str
     ) -> None:
+        query = Template(
+            "UPDATE $table SET first_name = ?, last_name = ? WHERE id = ?"
+        )
         self._connection.execute(
-            f"UPDATE {self.table_name}"
-            " SET first_name = ?,"
-            "last_name = ? WHERE id = ?",
+            query.substitute(table=self.table_name),
             (new_first_name, new_last_name, pk)
         )
         self._connection.commit()
 
     def delete(self, pk: int) -> None:
+        query = Template("DELETE FROM $table WHERE id = ?")
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id = ?",
+            query.substitute(table=self.table_name),
             (pk,)
         )
         self._connection.commit()
